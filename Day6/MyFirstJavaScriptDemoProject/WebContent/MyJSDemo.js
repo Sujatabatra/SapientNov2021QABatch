636b87db67377312c5f32fd89f110478067e40df
Sujatabatra <sujatabatra@hotmail.com>
2021-11-22 13:06:31 +0530

    Add files via upload

---
+++ Day6/MyFirstJavaScriptDemoProject/WebContent/MyJSDemo.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+function sum(){
+	var n1=window.prompt("Enter first No. ","0");
+	var n2=prompt("Enter Second No. " ,"0");
+	var sum=parseInt(n1)+parseInt(n2);
+	window.confirm("Sum : "+sum);
+}
