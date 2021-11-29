93fd958d56508a9349b72bd695b6876a6de8c871
Sujatabatra <sujatabatra@hotmail.com>
2021-11-29 10:23:05 +0530

    Add files via upload

---
+++ Day11 (Java)/MyStaticDemoProject/src/com/sujata/demo/ClassForStaticImport.java
@@ -0,0 +1,36 @@
+package com.sujata.demo;
+
+public class ClassForStaticImport {
+
+	public void method1() {
+		System.out.println("Hi I am method 1");
+	}
+	
+	static public void method2() {
+		System.out.println("Hi I am method 1");
+	}
+	public void method3() {
+		System.out.println("Hi I am method 1");
+	}
+	static public void method4() {
+		System.out.println("Hi I am method 1");
+	}
+	public void method5() {
+		System.out.println("Hi I am method 1");
+	}
+	public void method6() {
+		System.out.println("Hi I am method 1");
+	}
+	public void method7() {
+		System.out.println("Hi I am method 1");
+	}
+	static public void method8() {
+		System.out.println("Hi I am method 1");
+	}
+	public void method9() {
+		System.out.println("Hi I am method 1");
+	}
+	static public void method10() {
+		System.out.println("Hi I am method 1");
+	}
+}
---
+++ Day11 (Java)/MyStaticDemoProject/src/com/sujata/training/ClassForStaticImportMain.java
@@ -0,0 +1,17 @@
+package com.sujata.training;
+
+import static com.sujata.demo.ClassForStaticImport.*;
+
+import com.sujata.demo.ClassForStaticImport;
+
+public class ClassForStaticImportMain {
+
+	public static void main(String[] args) {
+		method2();
+		method8();
+		
+		ClassForStaticImport obj=new ClassForStaticImport();
+
+	}
+
+}
