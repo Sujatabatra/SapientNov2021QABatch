aea064422c89a1ac3092759f4ed30a7c5281cbc8
Sujatabatra <sujatabatra@hotmail.com>
2021-11-30 09:22:06 +0530

    Add files via upload

---
+++ Junit/MyFirstJUnitDemoProject/src/com/sujata/demo/Calculator.java
@@ -0,0 +1,17 @@
+package com.sujata.demo;
+
+public class Calculator {
+
+	//add is used to add any integer  number
+	public int add(int number1,int number2) {
+		return -1;
+	}
+	/*
+	 * divide is use to add any positive integer number,
+	 * if number is negative system will throw NegativeNumberException
+	 * Division by Zero will throw Arithmetic Exception
+	 */
+	public int divide(int number1,int number2) {
+		return 0;
+	}
+}
