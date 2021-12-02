d65bc5db269f3a1907f703c7ebcb8ee84c5bfaad
Sujatabatra <sujatabatra@hotmail.com>
2021-12-02 12:50:38 +0530

    Add files via upload

--- Day14 (Web Service)/MyFirstRestAssuredProject/src/com/sujata/test/MyFirstRestAssuredProject.java
+++ Day14 (Web Service)/MyFirstRestAssuredProject/src/com/sujata/test/MyFirstRestAssuredProject.java
@@ -2,8 +2,10 @@ package com.sujata.test;
 
 import static org.testng.Assert.assertEquals;
 
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.*;
@@ -28,13 +30,58 @@ public class MyFirstRestAssuredProject {
 	  assertEquals(response.getStatusCode(), 200);
 	  
   }
-//  @Test
+  @Test
   public void checkingForStatusTestCase() {
 	  given().
 	  get("https://simple-books-api.glitch.me/status")
 	  .then()
-	  .statusCode(200)
+	  .statusCode(200);
+//	  .log()
+//	  .all();
+  }
+  
+  @Test
+  public void checkForList() {
+	  given()
+	  .get("https://simple-books-api.glitch.me/books")
+	  .then()
+	  .statusCode(200);
+  }
+  
+  @Test
+  public void checkBookIdPositive() {
+	  given()
+	  .get("https://simple-books-api.glitch.me/books/4")
+	  .then()
+	  .statusCode(200);
+  }
+  
+  @Test
+  public void checkBookIdNegative() {
+	  given()
+	  .get("https://simple-books-api.glitch.me/books/9")
+	  .then()
+	  .statusCode(404);
+  }
+  
+  @Test
+  public void createOrderCheck() {
+	  JSONObject data=new JSONObject();
+	  
+	  data.put("bookId", 3);
+	  data.put("customerName", "Tufel");
+	  
+//	  System.out.println(data.toJSONString());
+	  given()
+	  .header("Authorization","Bearer 96377109fb456620170e539fdccbd350a623759707bec61663b6fb5c099b5c23")
+	  .contentType(ContentType.JSON)
+	  .body(data.toJSONString())
+	  .when()
+	  .post("https://simple-books-api.glitch.me/orders")
+	  .then()
+	  .statusCode(201)
 	  .log()
 	  .all();
   }
+  
 }
